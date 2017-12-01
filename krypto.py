83791428d1de28b68209140ba0f23faac675a76b
Magda Bukowinska <m.bukowinska@gmail.com>
2017-12-01 21:37:44 +0100

    initial

---
+++ krypto.py
@@ -0,0 +1,59 @@
+import falcon
+import json
+import requests
+
+def pobierz():
+    urls = {
+        'BTCPLN_Bitbay': 'https://bitbay.net/API/Public/BTCPLN/ticker.json',
+        'LTCPLN_Bitbay': 'https://bitbay.net/API/Public/LTCPLN/ticker.json',
+        'ETHPLN_Bitbay': 'https://bitbay.net/API/Public/ETHPLN/ticker.json',
+        'BCCPLN_Bitbay': 'https://bitbay.net/API/Public/BCCPLN/ticker.json',
+        'BTCPLN_BitMarket': 'https://www.bitmarket.pl/json/BTCPLN/ticker.json',
+        'LTCPLN_BitMarket': 'https://www.bitmarket.pl/json/LTCPLN/ticker.json',
+        'BCCPLN_BitMarket': 'https://www.bitmarket.pl/json/BCCPLN/ticker.json',
+        'BTC-EUR_Gdax': 'https://api.gdax.com/products/BTC-EUR/ticker',
+        'LTC-EUR_Gdax': 'https://api.gdax.com/products/LTC-EUR/ticker',
+        'ETH-EUR_Gdax': 'https://api.gdax.com/products/ETH-EUR/ticker',
+        'XXBTZEUR_Kraken': 'https://api.kraken.com/0/public/Ticker?pair=XBTEUR',
+        'XLTCZEUR_Kraken': 'https://api.kraken.com/0/public/Ticker?pair=LTCEUR',
+        'XETHZEUR_Kraken': 'https://api.kraken.com/0/public/Ticker?pair=ETHEUR',
+        'BCHEUR_Kraken': 'https://api.kraken.com/0/public/Ticker?pair=BCHEUR',
+
+    }
+
+    kursy={}
+
+    for kurs in urls.keys():
+        r = requests.get(url=urls[kurs])
+
+        # print(kurs, r.text)
+        result = json.loads(r.text)
+        # kursy[kurs]=int(result['last'])
+        if "Bitbay" in kurs or "BitMarket" in kurs:
+            wartosc = result['last']
+        elif "Gdax" in kurs:
+            wartosc = result['price']
+        elif "Kraken" in kurs:
+            # print(result)
+            wartosc = result['result'][kurs.split("_")[0]]['c'][0]
+        kursy[kurs] = wartosc
+
+
+    return kursy
+
+
+class KursyGen(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps(kursy)
+
+
+# falcon.API instances are callable WSGI apps
+#wsgi_app = api = falcon.API()
+
+# Resources are represented by long-lived class instances
+#handler = KursyGen()
+
+# things will handle all requests to the '/things' URL path
+#api.add_route('/', handler)
